73ced97b6b95d05a8ab4dcc9c89368b50b2797e3
Vu Pham <vupham0326@mail.com>
2015-05-08 23:28:38 +0000

    Adding comments to our web application

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,11 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
+
 	geocoded_by :address
 	after_validation :geocode
 
 	validates :name, length: { minimum: 3}
 	validates :address, :presence => true
 	validates :description, :presence => true
-
 end
