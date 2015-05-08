0d83a2e50d3126cb71277a3b73504fac39b50fa6
Vu Pham <vupham0326@mail.com>
2015-05-08 15:20:19 +0000

    Added more validations for address and description

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,9 @@ class Place < ActiveRecord::Base
 	belongs_to :user
 	geocoded_by :address
 	after_validation :geocode
-	
-	validates :name, :presence => true
+
+	validates :name, length: { minimum: 3}
+	validates :address, :presence => true
+	validates :description, :presence => true
 
 end
