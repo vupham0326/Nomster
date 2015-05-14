7a2a83146c229bb2f4fc777b18fe415120411979
Vu Pham <vupham0326@mail.com>
2015-05-14 08:35:30 +0000

    previewing the automated email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,15 @@
 Nomster::Application.configure do
-  config.action_mailer.default_url_options = { :host => 'localhost:3030' }
-  
+  config.action_mailer.delivery_method = :smtp
+ 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com", 
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD']
+  } 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -15,8 +24,8 @@ Nomster::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
